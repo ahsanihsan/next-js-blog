d09327f284e7ae6f2cca0e96318ed7c0cd403e19
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2021-08-22 21:30:50 +0500

    Added new page and global styles

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,14 @@
-const posts = () => {
-  return <div>Hello this is a post list</div>;
-};
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
-export default posts;
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>All Posts</title>
+      </Head>
+      <h1>Posts List</h1>
+    </Layout>
+  );
+}
